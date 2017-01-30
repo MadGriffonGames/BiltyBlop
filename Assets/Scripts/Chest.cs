fdd7dd8d170355c35aade9a2f779fc75f35e7ce4
MadGriffon <madgriffon.games@gmail.com>
2017-01-30 19:08:22 +0300

    Добавил ключ, дверь и сундук

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    private Coin rigidCoin;
+
+    private bool isEmpty = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidCoin = GameObject.FindObjectOfType<Coin>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!isEmpty && other.gameObject.tag == "Player")
+        {
+            isEmpty = true;
+            for (int i = 0; i < 10; i++)
+                Instantiate(rigidCoin, (this.gameObject.transform.position + new Vector3(0, 0, 0) ), Quaternion.identity);
+        }
+    }
+}

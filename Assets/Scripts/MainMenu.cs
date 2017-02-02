c5995bee815d03ffaaa02b7c57f7bc131d20ec0f
MadGriffon <madgriffon.games@gmail.com>
2017-02-02 11:43:30 +0300

    Изменил Level1 добавил скрипты в префабы

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public string sceneName { get; set; }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}

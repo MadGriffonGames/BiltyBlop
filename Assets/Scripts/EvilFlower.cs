a244ecccc5b1edcad720a208dd1cde45aa16d1c9
Chataev Gleb <chataev.gs@gmail.com>
2017-02-14 12:40:40 +0300

    добавлен Evil Flower и расширение Dragon Bones для Unity. удалены старые анимации.

---
+++ Assets/Scripts/EvilFlower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DragonBones;
+
+public class EvilFlower : MonoBehaviour 
+
+{
+	UnityArmatureComponent armature;
+
+    void Awake()
+    {
+		armature = GetComponent<UnityArmatureComponent> ();
+    }
+
+	public void Idle()
+	{
+		armature.animation.Play ("IDLE");
+	}
+
+	public void Attack()
+	{
+		armature.animation.timeScale = 1.5f;
+		armature.animation.Play ("ATTACK");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}

0e1852948b7862f05115212ede17661a76f0b14b
Chataev Gleb <chataev.gs@gmail.com>
2017-02-22 15:25:15 +0300

    Добавлен партикл RuneStone и внедрен в объект

--- Assets/Scripts/InteractiveObjects/RuneStone.cs
+++ Assets/Scripts/InteractiveObjects/RuneStone.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 
 public class RuneStone : InteractiveObject
 {
+	[SerializeField]
+	public Transform position;
+
+	[SerializeField]
+	private GameObject light;
 
     public override void Start()
     {
         base.Start();
+		position = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -24,6 +30,10 @@ public class RuneStone : InteractiveObject
             animator.SetTrigger("shine");
         }
     }
+	public void MakeFX()
+	{
+		Instantiate(light, position.localPosition + new Vector3(0, -0.5f, 1), Quaternion.Euler(new Vector3 (-90, 0 , 0)));
+	}
 
     public void ChangeScene()
     {

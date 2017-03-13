15feb6542d94336a6a7c2a56ebc349737dc15415
Chataev Gleb <chataev.gs@gmail.com>
2017-03-13 19:43:43 +0300

    сделал CrystalStar

---
+++ Assets/Star.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : InteractiveObject
+{
+	[SerializeField]
+	private GameObject crystalParticle;
+
+	public override void Start ()
+	{
+		base.Start();
+	}
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.transform.CompareTag("Player") && !other.transform.CompareTag("Sword"))
+		{
+			animator.SetTrigger("collected");
+			//GameManager.CollectedStars++;
+		}   
+	}
+
+	public void MakeFX()
+	{
+		Instantiate(crystalParticle, this.gameObject.transform.position + new Vector3(0, 0.2f, 1), Quaternion.Euler(new Vector3 (0, 0 , 0)));
+	}
+
+	public void DestroyObject()
+	{
+		SoundManager.PlaySound ("star_collect");
+		Destroy(this.gameObject);
+	}
+}

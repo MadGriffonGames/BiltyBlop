94c3d2d331e9bfa901227effc23e2d7834601469
chataev <chataev.gs@gmail.com>
2017-03-23 17:12:30 +0300

    moving spikes added

---
+++ Assets/Scripts/InteractiveObjects/MovingSpikesMedium.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSpikesMedium : InteractiveObject {
+
+	// Use this for initialization
+	public override void Start () {
+
+        base.Start();
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            animator.SetTrigger("enter");
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            animator.SetTrigger("exit");
+            animator.ResetTrigger("enter");        }
+    }
+
+}

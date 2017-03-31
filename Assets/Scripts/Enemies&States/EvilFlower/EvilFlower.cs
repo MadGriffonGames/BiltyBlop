fe1fc45abcace3a627706f79775ba8111a601787
chataev <chataev.gs@gmail.com>
2017-03-31 11:54:53 +0300

    лвл6 added EnemySight.cs Runic Grave Player controller переход из jump в fall Speed particle цвет

--- Assets/Scripts/Enemies&States/EvilFlower/EvilFlower.cs
+++ Assets/Scripts/Enemies&States/EvilFlower/EvilFlower.cs
@@ -50,6 +50,17 @@ public class EvilFlower : MeleeEnemy
         currentState.OnTriggerEnter2D(other);
     }
 
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("stay");
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("exit");
+        ChangeState(new EvilFlowerIdleState());
+    }
+
     public override IEnumerator TakeDamage()
     {
         health -= Player.Instance.damage;

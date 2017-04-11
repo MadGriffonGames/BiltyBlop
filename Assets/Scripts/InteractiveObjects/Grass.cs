e979f9e87b5e9a2c0252b366e06b1af797fb0e04
MadGriffon <madgriffon.games@gmail.com>
2017-04-11 12:10:56 +0300

    Коммит для Пряжкина

---
+++ Assets/Scripts/InteractiveObjects/Grass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DragonBones;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    protected UnityArmatureComponent armature;
+
+    void Start ()
+    {
+        armature = GetComponent<UnityArmatureComponent>();
+        armature.enabled = false;
+    }
+
+    private void OnBecameVisible()
+    {
+        armature.enabled = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        armature.enabled = false;
+    }
+}

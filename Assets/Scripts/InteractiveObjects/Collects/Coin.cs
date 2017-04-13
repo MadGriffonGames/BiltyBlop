57b53a89d461017d0fa6861c24fc399600c690a5
MadGriffon <madgriffon.games@gmail.com>
2017-04-13 18:24:01 +0300

    Оптимизировал монетки

--- Assets/Scripts/InteractiveObjects/Collects/Coin.cs
+++ Assets/Scripts/InteractiveObjects/Collects/Coin.cs
@@ -4,20 +4,47 @@ using UnityEngine;
 
 public class Coin : InteractiveObject
 {
-	public override void Start ()
+    [SerializeField]
+    SpriteRenderer mainCoin;
+    SpriteRenderer MySR;
+    bool collected = false;
+
+    public override void Start ()
     {
         base.Start();
-	}
+        MySR = GetComponent<SpriteRenderer>();
+        enabled = false;
+        MyAnimator.enabled = false;
+        if (mainCoin == null)
+        {
+            mainCoin = GameObject.FindGameObjectWithTag("MainCoin").GetComponent<SpriteRenderer>();
+        }
+    }
+
+    void Update()
+    {
+        if (!collected)
+        {
+            MySR.sprite = mainCoin.sprite;
+        }
+    }
 
     public void OnTriggerEnter2D(Collider2D other)
     {
         if(other.transform.CompareTag("Player") && !other.transform.CompareTag("Sword"))
         {
+            collected = true;
+            MyAnimator.enabled = true;
             MyAnimator.SetTrigger("collected");
             GameManager.CollectedCoins++;
             GameManager.lvlCollectedCoins++;
             SoundManager.PlaySound ("coin_collect");
         }   
+    }
+
+    private void OnBecameVisible()
+    {
+        enabled = true;
     }
 
     public void DestroyObject()

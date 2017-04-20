311955d90964f5ace31e5f54f9879fc0acb45451
MadGriffon <madgriffon.games@gmail.com>
2017-04-20 15:04:51 +0300

    Подредачил тригер дождя и 6 уровень

---
+++ Assets/Scripts/Environment/RainTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainTrigger : MonoBehaviour
+{
+    GameObject rainParticle;
+
+    private void Start()
+    {
+        rainParticle = GameObject.FindGameObjectWithTag("Rain");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player") && !rainParticle.activeInHierarchy)
+        {
+            rainParticle.SetActive(true);
+        }
+        else if (other.gameObject.CompareTag("Player") && rainParticle.activeInHierarchy)
+        {
+            rainParticle.SetActive(false);
+        }
+    }
+}

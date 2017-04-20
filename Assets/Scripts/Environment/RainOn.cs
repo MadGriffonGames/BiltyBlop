3e13eaa3abb485473b2e6c252c756e89725a12c7
MadGriffon <madgriffon.games@gmail.com>
2017-04-20 14:26:23 +0300

    Добавил дождь и дождевой задник

---
+++ Assets/Scripts/Environment/RainOn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainOn : MonoBehaviour
+{
+    GameObject rainParticle;
+
+    private void Start()
+    {
+        rainParticle = GameObject.FindGameObjectWithTag("Rain");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            rainParticle.transform.localPosition += new Vector3(0, 0, 1);
+        }
+    }
+}

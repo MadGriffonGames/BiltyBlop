7e1611f3f18fc67049bb10823cec800ed03f22f8
MadGriffon <madgriffon.games@gmail.com>
2017-04-25 17:55:42 +0300

    Начал делать падующее дерево и портал

---
+++ Assets/Scripts/InteractiveObjects/Portal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    GameObject nextPortal;
+    public BoxCollider2D bc;
+
+    private void Start()
+    {
+        bc = GetComponent<BoxCollider2D>();
+        Debug.Log(bc.size.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), Player.Instance.GetComponent<BoxCollider2D>(), true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Physics2D.IgnoreCollision(GetComponent<BoxCollider2D>(), Player.Instance.GetComponent<BoxCollider2D>(), false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Player.Instance.transform.position = nextPortal.transform.position + new Vector3(1 * Player.Instance.transform.localScale.x, -2f, 0);
+        }
+    }
+}

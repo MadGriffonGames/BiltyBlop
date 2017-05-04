f8279bce3f7bed31256890a4befdfebf93718842
spryazhkin <inwmustdie@gmail.com>
2017-05-04 16:42:34 +0300

    FX меча при ударах и корректный триггер дождя

--- Assets/SwordEdge.cs
+++ Assets/SwordEdge.cs
@@ -2,16 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwordEdge : MonoBehaviour {
+public class SwordEdge : MonoBehaviour
+{
 
-    private LineRenderer lineR;
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /*  private LineRenderer lineR;
+      private int i;
+      private bool isDrawing;
+      // Use this for initialization
+      void Start () {
+          i = 0;
+          lineR = gameObject.GetComponent<LineRenderer>();
+          lineR.material = new Material(Shader.Find("Mobile/Particles/Additive"));
+          lineR.startColor = new Color(255, 255, 255);
+          lineR.startWidth = 0f;
+          lineR.widthMultiplier = 1.2f;
+          lineR.endWidth = 0f;
+          lineR.numPositions = i;
+          isDrawing = false;
+      }
+
+      void Update () {
+
+         if (isDrawing)
+         {
+
+              lineR.numPositions = i+1; 
+              lineR.SetPosition(i, this.gameObject.transform.position);
+              i++;
+
+          }
+      }
+     */
+    private void Start()
+    {
+
+    }
+    public void StartDraw(float drawDur, float freezeDur)
+    {
+        GameObject child = new GameObject("SwordSlash");
+        child.transform.parent = this.gameObject.transform;
+        child.transform.localPosition = new Vector3(0f, 0f, -5f);
+        SwordSlash ssl = child.AddComponent<SwordSlash>();
+
+        StartCoroutine( ssl.StartDraw(drawDur, freezeDur));
+    }
+
 }

e8fe353fc259f80f5f23be5643ba01cfea1e103f
Chataev Gleb <chataev.gs@gmail.com>
2017-05-17 10:54:22 +0300

    tmp commit for Artem

---
+++ Assets/PlayVideo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent (typeof(AudioSource))]
+
+public class PlayVideo : MonoBehaviour {
+
+	public MovieTexture movie;
+	private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+
+		GetComponent<RawImage> ().texture = movie as MovieTexture;
+		audio = GetComponent<AudioSource> ();
+		audio.clip = movie.audioClip;
+		movie.Play ();
+		audio.Play ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

5da1d841ffec1ec3cbd5dc79670c77e75e9fdfd1
chataev <chataev.gs@gmail.com>
2017-05-22 14:23:33 +0300

    нормальная работа видео

---
+++ Assets/Scripts/Menu&UI/PlayVideo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayVideo : MonoBehaviour
+
+{
+
+    [SerializeField]
+    string movieName;
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey(movieName))
+        { 
+            Handheld.PlayFullScreenMovie(movieName + ".mp4", Color.black, FullScreenMovieControlMode.Hidden, FullScreenMovieScalingMode.AspectFit);
+            PlayerPrefs.SetString(movieName, "played");
+        } else Handheld.PlayFullScreenMovie(movieName + ".mp4", Color.black, FullScreenMovieControlMode.CancelOnInput,FullScreenMovieScalingMode.AspectFit);
+    }
+}

5756115f5fe39acc038c660514caca49dcd9ffa6
mikekrasilnikov <krasilnikovvmk@gmail.com>
2017-06-06 18:25:46 +0300

    Rain loop corrected. Добавлена функция воспроизведения звуков в лупе

--- Assets/Scripts/Player/GameManager.cs
+++ Assets/Scripts/Player/GameManager.cs
@@ -37,7 +37,9 @@ public class GameManager : MonoBehaviour
         coinTxt = FindObjectOfType<Text>();
         if((SceneManager.GetActiveScene().name != "MainMenu") && (SceneManager.GetActiveScene().name != "Level9"))
             SoundManager.PlayMusic ("kid_music", true);
-	}
+        if (SceneManager.GetActiveScene().name == "Level6")
+            SoundManager.PlaySoundLooped("rain sfx");
+    }
 
     public void PlayUISound(string sound)
     {

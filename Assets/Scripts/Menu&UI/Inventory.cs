72b0a50b6330b49d6accb69bfdb4626d7b6b717c
chataev <chataev.gs@gmail.com>
2017-06-20 13:39:42 +0300

    Inventory.cs + GameManager.cs (добалены новые PlayerPrefs)

---
+++ Assets/Scripts/Menu&UI/Inventory.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    /* Inventory Items Names */
+    public static string hpPots = "HealthPotCount";
+    public static string damageBonuses = "DamageBonusCount";
+    public static string speedBonuses = "SpeedBonusCount";
+    public static string timeBonuses = "TimeBonusCount";
+    public static string immortalBonuses = "ImmortalBonusCount";
+    public static string clips = "ClipsCount";
+
+    private static int upgradingValue = 5;
+
+
+    public bool AddItemToInventory(string itemName)
+    {
+        if (!PlayerPrefs.HasKey(itemName))
+        {
+            PlayerPrefs.SetInt(itemName, 1);
+            return true;
+        }
+        else if (PlayerPrefs.GetInt(itemName) < PlayerPrefs.GetInt("Max" + itemName))
+        {
+            PlayerPrefs.SetInt(itemName, PlayerPrefs.GetInt(itemName) + 1);
+            return true;
+        }
+        return false;
+    }
+
+    public bool RemoveItemToInventory(string itemName)
+    {
+        if (PlayerPrefs.GetInt(itemName) > 0)
+        {
+            PlayerPrefs.SetInt(itemName, PlayerPrefs.GetInt(itemName) - 1);
+            return true;
+        }
+        return false;
+    }
+
+    public void UpgradeStorage(string storageName, int cost)
+    {
+        PlayerPrefs.SetInt(storageName, PlayerPrefs.GetInt(storageName) + upgradingValue);
+        GameManager.CollectedCoins -= cost;
+    }
+
+}

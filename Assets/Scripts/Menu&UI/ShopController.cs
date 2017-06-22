987c59aafe0e259cfd794aab638c520b2c87d53c
chataev <chataev.gs@gmail.com>
2017-06-22 19:52:33 +0300

    продолжается апгрейд шопа

---
+++ Assets/Scripts/Menu&UI/ShopController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopController : MonoBehaviour {
+
+    public GameObject[] shops;
+    [SerializeField]
+    Button leftButton;
+    [SerializeField]
+    Button middleButton;
+    [SerializeField]
+    Button rightButton;
+
+    public string[] shopNames;
+    private int activeShopNumber;
+    private int shopsCount;
+    private static int buttonsCount = 3;
+
+	private void Start ()
+    {
+        shopsCount = shops.Length;
+        shopNames = new string[shopsCount];
+        for (int i = 0; i < shopsCount; i++)
+        {
+            shopNames[i] = shops[i].gameObject.name;
+        }
+        activeShopNumber = 1;
+
+        SetButtonText(rightButton, activeShopNumber + 1);
+        SetButtonText(middleButton, activeShopNumber);
+        SetButtonText(leftButton, activeShopNumber - 1);
+        shops[activeShopNumber].SetActive(true);
+    }
+
+
+    public void ChangeShopNames(int inc)
+    {
+        shops[activeShopNumber].SetActive(false);
+        activeShopNumber = (activeShopNumber + inc) % (shopsCount);
+        shops[activeShopNumber].SetActive(true);
+        
+        if (activeShopNumber == shopsCount - 1)  // n-1 n 1
+        {
+            SetButtonText(rightButton, 0);
+            SetButtonText(middleButton, shopsCount - 1);
+            SetButtonText(leftButton, shopsCount - 2);
+        }
+        else if (activeShopNumber == 0)          // n 1 2
+        {
+            SetButtonText(rightButton, 1);
+            SetButtonText(middleButton, 0);
+            SetButtonText(leftButton, shopsCount - 1);
+        }
+        else
+        {
+            SetButtonText(rightButton, activeShopNumber + 1);
+            SetButtonText(middleButton, activeShopNumber);
+            SetButtonText(leftButton, activeShopNumber - 1);
+        }
+
+    }
+
+    private void SetButtonText(Button button, int shopNumber)
+    {
+        button.GetComponentInChildren<Text>().text = shopNames[shopNumber];
+    }
+
+
+}
---
+++ Assets/SkinSwipeMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinSwipeMenu : SwipeMenu {
+
+
+    public Button applyButton;
+    public Button unlockButton;
+
+    public override void Start()
+    {
+        int buttonCount = buttons.Length;
+        distance = new float[buttonCount];
+        buttonDistance = (int)Mathf.Abs(buttons[1].GetComponent<RectTransform>().anchoredPosition.x - buttons[0].GetComponent<RectTransform>().anchoredPosition.x);
+
+        panel.anchoredPosition = new Vector2(buttons[SkinManager.Instance.NumberOfSkin(KidSkin.Instance.CurrentSkinName())].transform.position.x, panel.anchoredPosition.y);
+        minButtonsNumber = SkinManager.Instance.NumberOfSkin(KidSkin.Instance.CurrentSkinName());
+    }
+
+    public void UpdateSkinModel()
+    {
+        if (KidSkin.Instance.CurrentSkinName() != SkinManager.Instance.skinPrefabs[minButtonsNumber].name)
+        {
+            KidSkin.Instance.ChangeSkin(SkinManager.Instance.skinPrefabs[minButtonsNumber].name);
+            if (!SkinManager.Instance.isSkinUnlocked(minButtonsNumber))
+            {
+                applyButton.gameObject.SetActive(false);
+                unlockButton.gameObject.SetActive(true);
+                unlockButton.GetComponentInChildren<Text>().text = KidSkin.Instance.SkinCost().ToString();
+            }
+            else
+            {
+                applyButton.gameObject.SetActive(true);
+                unlockButton.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public override void LerpToButton(int position)
+    {
+        float newX = Mathf.Lerp(panel.anchoredPosition.x, position, Time.deltaTime * 10f);
+        Vector2 newPosition = new Vector2(newX, panel.anchoredPosition.y);
+        panel.anchoredPosition = newPosition;
+
+        if (Mathf.Abs(panel.anchoredPosition.x - position) < changingDistance)
+        {
+            UpdateSkinModel();
+            tapping = false;
+        }
+    }
+
+}

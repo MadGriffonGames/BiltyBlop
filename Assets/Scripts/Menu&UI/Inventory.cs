b828845210a38d78307b0669a8c2a3889abba0f9
chataev <chataev.gs@gmail.com>
2017-07-04 17:11:47 +0300

    Добавил функционал ItemShop и доработки инвентаря

--- Assets/Scripts/Menu&UI/Inventory.cs
+++ Assets/Scripts/Menu&UI/Inventory.cs
@@ -1,49 +1,145 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour {
 
+    private static Inventory instance;
+    public static Inventory Instance
+    {
+		get
+		{
+			if (instance == null)
+				instance = GameObject.FindObjectOfType<Inventory> ();
+			return instance;
+		}
+	}
+
     /* Inventory Items Names */
-    public static string hpPots = "HealthPotCount";
-    public static string damageBonuses = "DamageBonusCount";
-    public static string speedBonuses = "SpeedBonusCount";
-    public static string timeBonuses = "TimeBonusCount";
-    public static string immortalBonuses = "ImmortalBonusCount";
+    public static string hpPots = "HealthPot";
+    public static string damageBonuses = "DamageBonus";
+    public static string speedBonuses = "SpeedBonus";
+    public static string timeBonuses = "TimeBonus";
+    public static string immortalBonuses = "ImmortalBonus";
     public static string clips = "ClipsCount";
 
-    private static int upgradingValue = 5;
+    public string[] items;
 
+    const string max = "Max";
+    const string count = "Count";
+    const string costCoins = "CostCoins";
+    const string costCrystal = "CostCrystal";
 
-    public bool AddItemToInventory(string itemName)
+    const int upgradingValue = 5;
+
+    private void Start()
     {
-        if (!PlayerPrefs.HasKey(itemName))
+        items =new string[] { hpPots, damageBonuses, speedBonuses, timeBonuses, immortalBonuses, clips };
+
+        /* SETTING STARTING PARAMS FOR ITEMS */
+        SetStartingParamsForItem(hpPots, 10, 25, 2);
+        SetStartingParamsForItem(damageBonuses, 5, 50, 4);
+        SetStartingParamsForItem(speedBonuses, 5, 50, 4);
+        SetStartingParamsForItem(timeBonuses, 5, 50, 4);
+        SetStartingParamsForItem(immortalBonuses, 5, 50, 4);
+        SetStartingParamsForItem(clips, 5, 50, 4);
+    }
+
+    public bool BuyItem(string itemName, int itemCount, bool coinPayment)
+    {
+        if (CanAddItemToInventory(itemName, itemCount))
         {
-            PlayerPrefs.SetInt(itemName, 1);
+            /* 
+             
+                 ADD PAYMENT HERE 
+             
+                                     */
+
+            AddItemToInventory(itemName, itemCount);
             return true;
         }
-        else if (PlayerPrefs.GetInt(itemName) < PlayerPrefs.GetInt("Max" + itemName))
+        else return false;
+    }
+
+    private bool CanAddItemToInventory(string itemName, int itemCount)
+    {
+        if (!PlayerPrefs.HasKey(itemName + count))
         {
-            PlayerPrefs.SetInt(itemName, PlayerPrefs.GetInt(itemName) + 1);
+            PlayerPrefs.SetInt(itemName + count, 0);
+            return true;
+        }
+        else if (PlayerPrefs.GetInt(itemName + count) + itemCount < PlayerPrefs.GetInt(max + itemName))
+        {
             return true;
         }
         return false;
     }
 
-    public bool RemoveItemToInventory(string itemName)
+    public bool AddItemToInventory(string itemName, int itemCount) 
     {
-        if (PlayerPrefs.GetInt(itemName) > 0)
+        if (!PlayerPrefs.HasKey(itemName + count))
         {
-            PlayerPrefs.SetInt(itemName, PlayerPrefs.GetInt(itemName) - 1);
+            PlayerPrefs.SetInt(itemName + count, itemCount);
+            return true;
+        }
+        else if (PlayerPrefs.GetInt(itemName + count) < PlayerPrefs.GetInt(max + itemName))
+        {
+            PlayerPrefs.SetInt(itemName + count, PlayerPrefs.GetInt(itemName + count) + itemCount);
             return true;
         }
         return false;
     }
 
-    public void UpgradeStorage(string storageName, int cost)
+    public bool RemoveItemFromInventory(string itemName)  // = UseItem
     {
-        PlayerPrefs.SetInt(storageName, PlayerPrefs.GetInt(storageName) + upgradingValue);
+        if (PlayerPrefs.GetInt(itemName + count) > 0)
+        {
+            PlayerPrefs.SetInt(itemName + count, PlayerPrefs.GetInt(itemName + count) - 1);
+            return true;
+        }
+        return false;
+    }
+
+    public void UpgradeStorage(string itemName, int cost)
+    {
+        PlayerPrefs.SetInt(max + itemName + count, PlayerPrefs.GetInt(max + itemName + count) + upgradingValue);
         GameManager.CollectedCoins -= cost;
+    }
+
+    public int GetCoinCostOfItem(string itemName)
+    {
+        return PlayerPrefs.GetInt(itemName + costCoins);
+    }
+
+    public int GetCrystalCostOfItem(string itemName)
+    {
+        return PlayerPrefs.GetInt(itemName + costCrystal);
+    }
+
+    /* SETTING PLAYER PREFS METHODS */
+    private void SetStartingParamsForItem(string itemName, int maxCount, int coinCost, int crystalCost)
+    {
+        // Start Count = 0
+        if (!PlayerPrefs.HasKey(itemName + count))
+        {
+            PlayerPrefs.SetInt(itemName + count, 0);
+        }
+        // Max Count
+        if (!PlayerPrefs.HasKey(max + itemName))
+        {
+            PlayerPrefs.SetInt(max + itemName, maxCount);
+        }
+        // Cost in Coins
+        if (!PlayerPrefs.HasKey(itemName + costCoins))
+        {
+            PlayerPrefs.SetInt(itemName + costCoins, coinCost);
+        }
+        // Cost in Crystals
+        if (!PlayerPrefs.HasKey(itemName + costCrystal))
+        {
+            PlayerPrefs.SetInt(itemName + costCrystal, crystalCost);
+        }
     }
 
 }

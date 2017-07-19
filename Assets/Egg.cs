5b0b8b91fd03f45b0712f67612f656ee110b9ff6
mikekrasilnikov <krasilnikovvmk@gmail.com>
2017-07-19 14:25:10 +0300

    яйца

---
+++ Assets/Egg.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg : MonoBehaviour {
+
+    [SerializeField]
+    GameObject crashedEgg;
+    [SerializeField]
+    GameObject eggParticle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Sword"))
+        {
+            gameObject.SetActive(false);
+            Instantiate(eggParticle, this.gameObject.transform.position + new Vector3(0, 1f), Quaternion.identity);
+            crashedEgg.gameObject.SetActive(true);
+        }
+    }
+}

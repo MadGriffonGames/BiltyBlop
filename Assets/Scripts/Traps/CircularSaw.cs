af87dc962c1dda04654eaa7ed0cbf83409ceea02
MadGriffon <madgriffon.games@gmail.com>
2017-07-20 19:57:50 +0300

    Новые ловушки

---
+++ Assets/Scripts/Traps/CircularSaw.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularSaw : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float delay;
+    Animator myAnimator;
+
+    void Start ()
+    {
+        myAnimator = GetComponent<Animator>();
+        myAnimator.speed = speed;
+	}
+
+    IEnumerator Delay()
+    {
+        myAnimator.enabled = false;
+        yield return new WaitForSeconds(delay);
+        myAnimator.enabled = true;
+    }
+}

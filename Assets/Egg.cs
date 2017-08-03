f28eaf31e1b0e79da5923b864f96fbef6b65d5e6
mikekrasilnikov <krasilnikovvmk@gmail.com>
2017-08-03 16:27:01 +0300

    закончен 9 уровень, исправлен 13, 1 и 4. Яйца спавнят монетки

--- Assets/Egg.cs
+++ Assets/Egg.cs
@@ -8,10 +8,16 @@ public class Egg : MonoBehaviour {
     GameObject crashedEgg;
     [SerializeField]
     GameObject eggParticle;
+    [SerializeField]
+    public GameObject coinPack;
+    [SerializeField]
+    public int coinPackSize;
 
-	// Use this for initialization
-	void Start () {
-		
+    GameObject[] coins;
+
+    // Use this for initialization
+    void Start () {
+        ResetCoinPack();
 	}
 	
 	// Update is called once per frame
@@ -27,6 +33,36 @@ public class Egg : MonoBehaviour {
             Instantiate(eggParticle, this.gameObject.transform.position + new Vector3(0, 1f), Quaternion.identity);
             crashedEgg.gameObject.SetActive(true);
             SoundManager.PlaySound("egg_crack");
+            SpawnCoins(1, 2);
+        }
+    }
+
+    public void SpawnCoins(int min, int max)
+    {
+        if (coinPackSize != 0)
+        {
+            int spawnCount = UnityEngine.Random.Range(min, max);
+            spawnCount = coinPackSize > spawnCount ? spawnCount : coinPackSize;
+            coinPackSize -= spawnCount;
+            for (int i = 0; i < spawnCount; i++)
+            {
+                coins[i].SetActive(true);
+                coins[i].GetComponent<Rigidbody2D>().velocity = new Vector2(UnityEngine.Random.Range(-2f, 2f), 3.5f);
+                coins[i].transform.parent = null;
+                coins[i].transform.localScale = new Vector2(0.78f, 0.78f);
+            }
+        }
+    }
+
+    public void ResetCoinPack()
+    {
+        coins = new GameObject[coinPackSize];
+        if (coinPackSize != 0)
+        {
+            for (int i = 0; i < coinPack.transform.childCount; i++)
+            {
+                coins[i] = coinPack.transform.GetChild(i).gameObject;
+            }
         }
     }
 }

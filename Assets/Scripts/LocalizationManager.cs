7a4c224ecc689ad322ccce59439f4753217eef07
MadGriffon <madgriffon.games@gmail.com>
2017-11-23 14:49:34 +0300

    Начал делать локализацию

---
+++ Assets/Scripts/LocalizationManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LocalizationManager : MonoBehaviour
+{
+    private static LocalizationManager instance;
+    public static LocalizationManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = GameObject.FindObjectOfType<LocalizationManager>();
+            return instance;
+        }
+    }
+
+    string pathToJson;
+    string language = "RU";
+    string jsonString;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        pathToJson = Application.streamingAssetsPath + "/" + language + ".json";
+        jsonString = File.ReadAllText(pathToJson);
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}

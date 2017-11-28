96bfd0a0aa1a4b603395cdfae28a89d0c8890da3
mkrasilnikov <krasilnikovvmk@gmail.com>
2017-11-28 17:16:13 +0300

    подвинул влево второго босса, исправил коллизию могилы и стенки, поменял спрайт в ачивках, опустил платформы

--- Assets/Scripts/Enemies&States/Spider/SpiderWall.cs
+++ Assets/Scripts/Enemies&States/Spider/SpiderWall.cs
@@ -25,7 +25,7 @@ public class SpiderWall : InteractiveObject {
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.transform.CompareTag("Sword") || other.transform.CompareTag("Throwing"))
+        if (other.transform.CompareTag("Sword") || other.transform.CompareTag("Throwing") || other.transform.CompareTag("grave"))
         {
             CameraEffect.Shake(0.2f, 0.2f);
             Instantiate(groundParticle, this.gameObject.transform.position + new Vector3(0, 0.5f, -3), Quaternion.identity);

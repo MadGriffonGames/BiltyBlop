b8cc196890336210e13835c0dbedb8d128aca03b
Chataev Gleb <chataev.gs@gmail.com>
2017-12-13 14:23:00 +0300

    Restore Purchases

--- Assets/Scripts/PurchaseManager.cs
+++ Assets/Scripts/PurchaseManager.cs
@@ -332,6 +332,16 @@ public class PurchaseManager : MonoBehaviour, IStoreListener
 
         m_StoreController = controller;
         m_StoreExtensionProvider = extensions;
+		#if UNITY_IOS
+		extensions.GetExtension<IAppleExtensions> ().RestoreTransactions (result => {
+			if (result) {
+				// This does not mean anything was restored,
+				// merely that the restoration process succeeded.
+			} else {
+				// Restoration failed.
+			}
+		});
+		#endif
     }
 
     public void OnInitializeFailed(InitializationFailureReason error)

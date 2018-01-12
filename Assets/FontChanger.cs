066a2f8b9921adeab4a7f31e5a5eed32100f51bc
MadGriffon <madgriffon.games@gmail.com>
2018-01-12 16:14:42 +0300

    Доделал уровень 23

--- Assets/FontChanger.cs
+++ Assets/FontChanger.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEditor.SceneManagement;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine.UI;
+//using UnityEditor.SceneManagement;
+//using UnityEngine;
 
-public class FontChanger : MonoBehaviour
-{
-    Text[] tmp;
-    Font font;
+//public class FontChanger : MonoBehaviour
+//{
+//    Text[] tmp;
+//    Font font;
 
-    [ContextMenu("ChangeFonts")]
-    private void ChangeFonts()
-    {
-        font = (Font)Resources.Load("Gilroy-Black");
+//    [ContextMenu("ChangeFonts")]
+//    private void ChangeFonts()
+//    {
+//        font = (Font)Resources.Load("Gilroy-Black");
 
-        tmp = FindObjectsOfType<Text>();
-        for (int i = 0; i < tmp.Length; i++)
-        {
-            tmp[i].font = font;
-        }
-    }
-}
+//        tmp = FindObjectsOfType<Text>();
+//        for (int i = 0; i < tmp.Length; i++)
+//        {
+//            tmp[i].font = font;
+//        }
+//    }
+//}

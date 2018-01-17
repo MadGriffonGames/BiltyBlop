9088fb4041069e99b645c24af042c159260f86f8
MadGriffon <madgriffon.games@gmail.com>
2018-01-17 15:44:19 +0300

    Доделал тролля и переходы на 3 акт

--- Assets/Scripts/Enemies&States/Troll/Troll.cs
+++ Assets/Scripts/Enemies&States/Troll/Troll.cs
@@ -11,11 +11,29 @@ public class Troll : MovingMeleeEnemy
     [SerializeField]
     public GameObject blow;
     [SerializeField]
-    GameObject bomb;
+    public GameObject bomb;
+    [SerializeField]
+    private float shootingRange;
     bool damaged = false;
     public bool walk = false;
+    [HideInInspector]
+    public bool canAttack;
+    [HideInInspector]
+    public bool isTimerTick;
+    float timer;
 
-    
+    public bool InShootingRange
+    {
+        get
+        {
+            if (Target != null)//if enemy has a target
+            {
+                //return distance between enemy and target <= meleeRange (true or false)
+                return Vector2.Distance(transform.position, Target.transform.position) <= shootingRange;
+            }
+            return false;
+        }
+    }
 
     void Awake()
     {
@@ -28,7 +46,10 @@ public class Troll : MovingMeleeEnemy
     {
         base.Start();
 
+        canAttack = true;
         isAttacking = false;
+        isTimerTick = false;
+        timer = 0;
         ChangeState(new TrollPatrolState());
     }
 
@@ -37,6 +58,17 @@ public class Troll : MovingMeleeEnemy
         if (!IsDead)
         {           
             LookAtTarget();
+
+            if (isTimerTick)
+            {
+                timer += Time.deltaTime;
+            }
+            if (timer >= 2f)
+            {
+                isTimerTick = false;
+                timer = 0;
+                canAttack = true;
+            }
         }
         if (!TakingDamage && !Attack)
         {

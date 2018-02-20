6700145338c8a220425a3c05e1d6c4a579714f6c
mkrasilnikov <krasilnikovvmk@gmail.com>
2018-02-20 17:39:20 +0300

    туплоковский ыфыкс

--- Assets/Scripts/Camera/CameraEffect.cs
+++ Assets/Scripts/Camera/CameraEffect.cs
@@ -10,9 +10,19 @@ public class CameraEffect : MonoBehaviour
     private static float shakeElapsed, shakeDuration, initiatePower, percentComplete;
     private float bloodElapsed, bloodDuration;
     GameObject blood;
+    GameObject backTyplak;
+    Image backTyplackImage;
+    GameObject cloudsTyplak;
+    Image cloudsTyplakImage;
     Color origColor;
+    GameObject typlakViniette;
+    Image typlakVinietteImage;
     bool hide = false;
+    float colorShift = 0.01f;
+    float sizeShift = 0.005f;
+    float vinietteColorShift;
 
+    public static bool isTyplakEffect = false;
     public static bool changeColors;
     PostProcessingProfile myProfile;
     float hueShift = 0;
@@ -23,6 +33,12 @@ public class CameraEffect : MonoBehaviour
         percentComplete = 1;
         position = GetComponent<Transform>();
         blood = UI.Instance.transform.Find("Blood").gameObject;
+        backTyplak = UI.Instance.transform.Find("BackTyplak").gameObject;
+        cloudsTyplak = UI.Instance.transform.Find("CloudsTyplak").gameObject;
+        typlakViniette = UI.Instance.transform.Find("TyplakViniette").gameObject;
+        typlakVinietteImage = typlakViniette.GetComponent<Image>();
+        backTyplackImage = backTyplak.GetComponent<Image>();
+        cloudsTyplakImage = cloudsTyplak.GetComponent<Image>();
         myProfile = GetComponent<PostProcessingBehaviour>().profile;
         myProfile.colorGrading.enabled = false;
         myProfile.chromaticAberration.enabled = false;
@@ -108,7 +124,20 @@ public class CameraEffect : MonoBehaviour
                 myProfile.colorGrading.enabled = true;
                 myProfile.chromaticAberration.enabled = true;
             }
+            else
+            {
+                if (!isTyplakEffect)
+                {
+                    EnableTyplakEffect();
+                }
+                StartMoving();
+            }
 
+        }
+
+        if (isTyplakEffect)
+        {
+            StartMoving();
         }
     }
 
@@ -118,6 +147,10 @@ public class CameraEffect : MonoBehaviour
         {
             myProfile.colorGrading.enabled = false;
             myProfile.chromaticAberration.enabled = false;
+        }
+        else
+        {
+            DisableTyplakEffect();
         }
     }
 
@@ -133,5 +166,59 @@ public class CameraEffect : MonoBehaviour
             myProfile.chromaticAberration.enabled = false;
             myProfile.grain.enabled = false;
         }
+    }
+
+    public void EnableTyplakEffect()
+    {
+        backTyplak.SetActive(true);
+        //cloudsTyplak.SetActive(true);
+        typlakViniette.SetActive(true);
+        isTyplakEffect = true;
+    }
+
+    void StartMoving()
+    {
+        backTyplackImage.color += new Color(0, 0, 0, colorShift);
+        //cloudsTyplakImage.color += new Color(0, 0, 0, colorShift * 1.5f);
+        if (backTyplackImage.color.a >= 1)
+        {
+            colorShift = -0.005f;
+        }
+        if (backTyplackImage.color.a <= 0.6f)
+        {
+            colorShift = 0.005f;
+        }
+
+        if (typlakVinietteImage.color.a <= 0.2f)
+        {
+            vinietteColorShift = 0.025f;
+        }
+        if (typlakVinietteImage.color.a >= 0.6f)
+        {
+            vinietteColorShift = -0.025f;
+        }
+
+        if (typlakViniette.GetComponent<RectTransform>().localScale.x > 1.1f)
+        {
+            sizeShift = -0.015f;
+        }
+
+        if (typlakViniette.GetComponent<RectTransform>().localScale.x < 0.9f)
+        {
+            sizeShift = 0.015f;
+        }
+
+        typlakVinietteImage.color += new Color(0, 0, 0, vinietteColorShift);
+        typlakViniette.GetComponent<RectTransform>().localScale += new Vector3(sizeShift, sizeShift);
+
+        //cloudsTyplak.GetComponent<RectTransform>().localScale += new Vector3(sizeShift , -sizeShift);
+        //cloudsTyplak.GetComponent<RectTransform>().position += new Vector3(sizeShift * 3, 0);
+    }
+
+    public void DisableTyplakEffect()
+    {
+        backTyplak.SetActive(false);
+        cloudsTyplak.SetActive(false);
+        isTyplakEffect = false;
     }
 }
